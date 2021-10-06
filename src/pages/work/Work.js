0b343af09df139b6f23f03f31a8db0f379daf1f4
Adam Erickson <aerickson@dmsi.com>
2021-10-05 21:00:40 -0500

    Fixed '01' to '04' in nav which was a find/replace error.

--- src/pages/work/Work.js
+++ src/pages/work/Work.js
@@ -48,7 +48,7 @@ const Work = () => {
           <li className="selected">01.</li>
           <li>02.</li>
           <li>03.</li>
-          <li>01.</li>
+          <li>04.</li>
           <li>05.</li>
           <li>06.</li>
           <li>07.</li>
